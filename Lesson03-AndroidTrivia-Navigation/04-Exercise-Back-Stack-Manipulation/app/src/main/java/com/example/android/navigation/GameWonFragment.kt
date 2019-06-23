e6c26369efba62b2769a7f3819d652c9d85193cc
Yini <ydxb7@mail.missouri.edu>
2019-06-22 20:41:57 -0700

    update

---
+++ Lesson03-AndroidTrivia-Navigation/04-Exercise-Back-Stack-Manipulation/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.navigation
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import androidx.databinding.DataBindingUtil
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.core.app.ShareCompat
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.example.android.navigation.databinding.FragmentGameWonBinding
+import android.content.pm.ResolveInfo
+import android.content.pm.PackageManager
+
+
+
+class GameWonFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_game_won, container, false)
+        // TODO (06) Add an onClick Handler for the nextMatch button that navigates to the gameFragment
+        // using action_gameWonFragment_to_gameFragment
+        return binding.root
+    }
+}

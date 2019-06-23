4d96b41b26a600a7e6a97c24c375e8a5f0ba1525
Yini <ydxb7@mail.missouri.edu>
2019-06-22 22:39:16 -0700

    L03.04 Back Stack Manipulation: 1. Navigate back 并跳过1个fragmen。 2. 加上2个action，也是同样的功能，pop back and skip a fragment.

--- Lesson03-AndroidTrivia-Navigation/04-Exercise-Back-Stack-Manipulation/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ Lesson03-AndroidTrivia-Navigation/04-Exercise-Back-Stack-Manipulation/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -44,6 +44,9 @@ class GameWonFragment : Fragment() {
                 inflater, R.layout.fragment_game_won, container, false)
         // TODO (06) Add an onClick Handler for the nextMatch button that navigates to the gameFragment
         // using action_gameWonFragment_to_gameFragment
+        binding.nextMatchButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+        }
         return binding.root
     }
 }

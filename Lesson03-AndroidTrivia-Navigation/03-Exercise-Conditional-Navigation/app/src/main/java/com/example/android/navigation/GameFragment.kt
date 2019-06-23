d6f9f067a2fe487cdcbfe5a39354f11da0dfb6ea
Yini <ydxb7@mail.missouri.edu>
2019-06-22 20:38:52 -0700

    L03.03 Conditional Navigation: navigate 1 fragment to 2 different fragments with different conditions.

--- Lesson03-AndroidTrivia-Navigation/03-Exercise-Conditional-Navigation/app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ Lesson03-AndroidTrivia-Navigation/03-Exercise-Conditional-Navigation/app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -99,14 +99,14 @@ class GameFragment : Fragment() {
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
                         // TODO (05) Find the navController from the view and navigate to the gameWonFragment
-                        // call view.findNavController
-                        // navigate to R.id.action_gameFragment_to_gameWonFragment
+                        // call view.findNavController navigate to R.id.action_gameFragment_to_gameWonFragment
+                        view.findNavController().navigate(R.id.action_gameFragment_to_gameWonFragment)
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
                     // TODO (06) Find the navController from the view and navigate to the gameOverFragment
-                    // call view.findNavController
-                    // navigate to R.id.action_gameFragment_to_gameOverFragment
+                    // call view.findNavController navigate to R.id.action_gameFragment_to_gameOverFragment
+                    view.findNavController().navigate(R.id.action_gameFragment_to_gameOverFragment2)
                 }
             }
         }

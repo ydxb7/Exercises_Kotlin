b2feae481ee7ba2fc3dc0279a3ac69de2efa8331
Yini <ydxb7@mail.missouri.edu>
2019-06-23 03:49:58 -0700

    08 Sharing Success: 增加“分享”信息功能，如果可以处理“分享inent”就显示“分享”按钮，否则就不显示

--- Lesson03-AndroidTrivia-Navigation/08-Exercise-Sharing-Success/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ Lesson03-AndroidTrivia-Navigation/08-Exercise-Sharing-Success/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -52,15 +52,53 @@ class GameWonFragment : Fragment() {
                 Toast.LENGTH_LONG).show()
         // TODO (01) Add setHasOptionsMenu(true)
         // This allows onCreateOptionsMenu to be called
+        setHasOptionsMenu(true)
         return binding.root
     }
 
 
     // TODO (02) Create getShareIntent method
+    private fun getShareIntent() : Intent {
+        var args = GameWonFragmentArgs.fromBundle(arguments!!)
+
+//        val shareIntent = Intent(Intent.ACTION_SEND)
+//        shareIntent.setType("text/plain")
+//                .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+//        return shareIntent
+
+        return ShareCompat.IntentBuilder.from(activity)
+                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+                .setType("text/plain")
+                .intent
+
+    }
+
     // TODO (03) Create shareSuccess method
+    private fun shareSuccess(){
+        startActivity(getShareIntent())
+    }
+
     // TODO (04) Override and fill out onCreateOptionsMenu
     // Inflate the winner_menu and set the share menu item to invisible if the activity doesn't
     // resolve
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.winner_menu, menu)
+        // check if the activity resolves
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            // hide the menu item if it doesn't resolve
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+
     // TODO (05) Override onOptionsItemSelected
     // Call the shareSuccess method when the item id matches R.id.share
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            R.id.share -> shareSuccess()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }

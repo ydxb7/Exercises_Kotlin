a38d6fe2c74ab21fd7cc86c829c4de6896e8c288
Yini <ydxb7@mail.missouri.edu>
2019-06-23 02:13:22 -0700

    L03.07 Adding Safe Arguments: 使用safe args，用bundle在不同fragment中pass message。并且用safe args获取R.id...

--- Lesson03-AndroidTrivia-Navigation/07-Exercise-Adding-Safe-Arguments/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ Lesson03-AndroidTrivia-Navigation/07-Exercise-Adding-Safe-Arguments/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -45,10 +45,16 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener { view: View ->
             // TODO (10) Replace action ID with actionGameWonFragmentToGameFragment
             // From GameWonFragmentDirections
-            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+//            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+            view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
         // TODO (08) Add and show toast to get the GameWonFragmentArgs from the arguments Bundle
         // "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}"
+        var args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context,
+                "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
+                Toast.LENGTH_LONG).show()
+
         return binding.root
     }
 }

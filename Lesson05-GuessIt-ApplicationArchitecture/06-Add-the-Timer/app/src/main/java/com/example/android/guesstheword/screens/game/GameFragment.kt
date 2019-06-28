6fc5d7f9e93ee7d80cb52cd41570d88e5672f01f
Yini <ydxb7@mail.missouri.edu>
2019-06-28 06:29:23 -0700

    L05.06 Add the Timer: CountDownTimer 增加一个倒计时功能，倒计时结束game finish

--- Lesson05-GuessIt-ApplicationArchitecture/06-Add-the-Timer/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ Lesson05-GuessIt-ApplicationArchitecture/06-Add-the-Timer/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,6 +17,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -69,6 +70,9 @@ class GameFragment : Fragment() {
 
         // TODO (07) Setup an observer relationship to update binding.timerText
         // You can use DateUtils.formatElapsedTime to correctly format the long to a time string
+        viewModel.currentTime.observe(this, Observer { newTime ->
+            binding.timerText.text = DateUtils.formatElapsedTime(newTime)
+        })
 
         // Sets up event listening to navigate the player when the game is finished
         viewModel.eventGameFinish.observe(this, Observer { isFinished ->
--- Lesson05-GuessIt-ApplicationArchitecture/06-Add-the-Timer/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Lesson05-GuessIt-ApplicationArchitecture/06-Add-the-Timer/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -27,11 +28,25 @@ import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
 
     // TODO (01) Copy over the provided companion object with the timer constants
+    companion object {
+        // These represent different important times
+        // This is when the game is over
+        const val DONE = 0L
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+        // This is the total time of the game
+        const val COUNTDOWN_TIME = 10000L
+    }
 
     // TODO (02) Create a timer field of type CountDownTimer
+    private val timer: CountDownTimer
+
 
     // TODO (03) Create a properly encapsulated LiveData for the current time called currentTime
     // Its type should be Long
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
 
     // The current word
     private val _word = MutableLiveData<String>()
@@ -60,6 +75,21 @@ class GameViewModel : ViewModel() {
 
         // TODO (04) Copy over the CountDownTimer code and then update currentTime and
         // eventGameFinish appropriately as the timer ticks and finishes
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                // TODO implement what should happen each tick of the timer
+                _currentTime.value = (millisUntilFinished / ONE_SECOND)
+            }
+
+            override fun onFinish() {
+                // TODO implement what should happen when the timer finishes
+                _currentTime.value = DONE
+                _eventGameFinish.value = true
+            }
+        }
+
+        timer.start()
     }
 
     /**
@@ -100,10 +130,11 @@ class GameViewModel : ViewModel() {
         if (wordList.isEmpty()) {
             // TODO (05) Update this logic so that the game doesn't finish;
             // Instead the list is reset and re-shuffled when you run out of words
-            _eventGameFinish.value = true
-        } else {
-            _word.value = wordList.removeAt(0)
+//            _eventGameFinish.value = true
+            resetList()
         }
+        _word.value = wordList.removeAt(0)
+
     }
 
     /** Methods for buttons presses **/
@@ -125,5 +156,9 @@ class GameViewModel : ViewModel() {
     }
 
     // TODO (06) Cancel the timer in onCleared
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+    }
 
 }

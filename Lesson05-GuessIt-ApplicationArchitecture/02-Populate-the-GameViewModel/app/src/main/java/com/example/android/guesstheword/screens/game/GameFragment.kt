755b79f48bae33f337987272b9dc5a9272b61179
Yini <ydxb7@mail.missouri.edu>
2019-06-28 02:04:23 -0700

    update

---
+++ Lesson05-GuessIt-ApplicationArchitecture/02-Populate-the-GameViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.guesstheword.screens.game
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import com.example.android.guesstheword.R
+import com.example.android.guesstheword.databinding.GameFragmentBinding
+
+/**
+ * Fragment where the game is played
+ */
+class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
+
+    // TODO (01) Move over the word, score and wordList variables to the GameViewModel
+    // The current word
+    private var word = ""
+
+    // The current score
+    private var score = 0
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    private lateinit var binding: GameFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.game_fragment,
+                container,
+                false
+        )
+
+        // Get the viewmodel
+        Log.i("GameFragment", "Called ViewModelProviders.of")
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+
+        // TODO (03) Move over this initialization to the GameViewModel
+        resetList()
+        nextWord()
+
+        // TODO (04) Update these onClickListeners to refer to call methods in the ViewModel then
+        // update the UI
+        binding.correctButton.setOnClickListener { onCorrect() }
+        binding.skipButton.setOnClickListener { onSkip() }
+        updateScoreText()
+        updateWordText()
+        return binding.root
+
+    }
+
+    // TODO (02) Move over methods resetList, nextWord, onSkip and onCorrect to the GameViewModel
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Called when the game is finished
+     */
+    private fun gameFinished() {
+        val action = GameFragmentDirections.actionGameToScore(score)
+        findNavController(this).navigate(action)
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+        updateWordText()
+        updateScoreText()
+    }
+
+    /** Methods for buttons presses **/
+
+    private fun onSkip() {
+        score--
+        nextWord()
+    }
+
+    private fun onCorrect() {
+        score++
+        nextWord()
+    }
+
+    /** Methods for updating the UI **/
+
+    // TODO (05) Update these methods to get word and score from the viewmodel
+    private fun updateWordText() {
+        binding.wordText.text = word
+
+    }
+
+    private fun updateScoreText() {
+        binding.scoreText.text = score.toString()
+    }
+}

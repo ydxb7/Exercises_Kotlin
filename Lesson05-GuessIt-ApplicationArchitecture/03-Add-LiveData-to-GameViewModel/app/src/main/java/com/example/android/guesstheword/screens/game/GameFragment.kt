dcc17b33421b5accae0a2a11cc18e86def798b4d
Yini <ydxb7@mail.missouri.edu>
2019-06-28 02:55:02 -0700

    update

---
+++ Lesson05-GuessIt-ApplicationArchitecture/03-Add-LiveData-to-GameViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.guesstheword.screens.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import com.example.android.guesstheword.R
+import com.example.android.guesstheword.databinding.GameFragmentBinding
+
+/**
+ * Fragment where the game is played
+ */
+class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
+
+    private lateinit var binding: GameFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.game_fragment,
+                container,
+                false
+        )
+
+        // Get the viewmodel
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+
+        binding.correctButton.setOnClickListener {
+            viewModel.onCorrect()
+            updateScoreText()
+            updateWordText()
+        }
+        binding.skipButton.setOnClickListener {
+            viewModel.onSkip()
+            updateScoreText()
+            updateWordText()
+        }
+
+        // TODO (04) Setup the LiveData observation relationship by getting the LiveData from your
+        // ViewModel and calling observe. Make sure to pass in *this* and then an Observer lambda
+        updateScoreText()
+        updateWordText()
+        return binding.root
+
+    }
+
+    /**
+     * Called when the game is finished
+     */
+    fun gameFinished() {
+        // TODO (06) Add a null safety check here - you can use the elvis operator to pass 0 if
+        // the LiveData is null
+        val action = GameFragmentDirections.actionGameToScore(viewModel.score)
+        findNavController(this).navigate(action)
+    }
+
+    /** Methods for updating the UI **/
+
+    // TODO (05) Move this code to update the UI up to your Observers; remove references to
+    // updateWordText and updateScoreText - you shouldn't need them!
+    private fun updateWordText() {
+        binding.wordText.text = viewModel.word
+    }
+
+    private fun updateScoreText() {
+        binding.scoreText.text = viewModel.score.toString()
+    }
+}

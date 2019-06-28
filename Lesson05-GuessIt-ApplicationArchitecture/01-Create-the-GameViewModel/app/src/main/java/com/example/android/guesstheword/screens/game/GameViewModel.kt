5aec23a69bb003ea632f16d3e2991ece87511c56
Yini <ydxb7@mail.missouri.edu>
2019-06-28 01:59:45 -0700

    L05.01 Create the GameViewModel: 用 ViewModel 来数据与界面分离，同时会自动处理屏幕旋转,暂时离开界面数据不丢失的情况. 用ViewModelProviders创建fragment和viewModel的联系

--- Lesson05-GuessIt-ApplicationArchitecture/01-Create-the-GameViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Lesson05-GuessIt-ApplicationArchitecture/01-Create-the-GameViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -16,5 +16,19 @@
 
 package com.example.android.guesstheword.screens.game
 
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
 // TODO (02) Create the GameViewModel class, extending ViewModel
-// TODO (03) Add init and override onCleared; Add log statements to both
+class GameViewModel : ViewModel(){
+    // TODO (03) Add init and override onCleared; Add log statements to both
+    init {
+        Log.i("GameViewModel", "GameViewModel created!")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed!")
+    }
+
+}

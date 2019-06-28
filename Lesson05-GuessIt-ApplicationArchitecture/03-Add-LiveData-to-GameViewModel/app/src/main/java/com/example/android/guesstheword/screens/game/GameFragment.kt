8bcded243075542bc9a0d65530421a47e907cc41
Yini <ydxb7@mail.missouri.edu>
2019-06-28 04:00:56 -0700

    L05.03 Add LiveData to GameViewModel: LiveData: observe 变量，当他有变化时可以自动call methods，这样可以让变量改变时自动更新屏幕上的数据

--- Lesson05-GuessIt-ApplicationArchitecture/03-Add-LiveData-to-GameViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ Lesson05-GuessIt-ApplicationArchitecture/03-Add-LiveData-to-GameViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
@@ -52,19 +53,27 @@ class GameFragment : Fragment() {
 
         binding.correctButton.setOnClickListener {
             viewModel.onCorrect()
-            updateScoreText()
-            updateWordText()
+//            updateScoreText()
+//            updateWordText()
         }
         binding.skipButton.setOnClickListener {
             viewModel.onSkip()
-            updateScoreText()
-            updateWordText()
+//            updateScoreText()
+//            updateWordText()
         }
 
         // TODO (04) Setup the LiveData observation relationship by getting the LiveData from your
         // ViewModel and calling observe. Make sure to pass in *this* and then an Observer lambda
-        updateScoreText()
-        updateWordText()
+        viewModel.score.observe(this, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+
+        viewModel.word.observe(this, Observer { newWord ->
+            binding.wordText.text = newWord
+        })
+
+//        updateScoreText()
+//        updateWordText()
         return binding.root
 
     }
@@ -75,7 +84,7 @@ class GameFragment : Fragment() {
     fun gameFinished() {
         // TODO (06) Add a null safety check here - you can use the elvis operator to pass 0 if
         // the LiveData is null
-        val action = GameFragmentDirections.actionGameToScore(viewModel.score)
+        val action = GameFragmentDirections.actionGameToScore(viewModel.score.value ?: 0)
         findNavController(this).navigate(action)
     }
 
@@ -83,11 +92,11 @@ class GameFragment : Fragment() {
 
     // TODO (05) Move this code to update the UI up to your Observers; remove references to
     // updateWordText and updateScoreText - you shouldn't need them!
-    private fun updateWordText() {
-        binding.wordText.text = viewModel.word
-    }
+//    private fun updateWordText() {
+//        binding.wordText.text = viewModel.word
+//    }
 
-    private fun updateScoreText() {
-        binding.scoreText.text = viewModel.score.toString()
-    }
+//    private fun updateScoreText() {
+//        binding.scoreText.text = viewModel.score.toString()
+//    }
 }
--- Lesson05-GuessIt-ApplicationArchitecture/03-Add-LiveData-to-GameViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Lesson05-GuessIt-ApplicationArchitecture/03-Add-LiveData-to-GameViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.guesstheword.screens.game
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 
@@ -26,10 +27,10 @@ class GameViewModel : ViewModel() {
 
     // TODO (01) Wrap word and score in MutableLiveData
     // The current word
-    var word = ""
+    var word = MutableLiveData<String>()
 
     // The current score
-    var score = 0
+    var score = MutableLiveData<Int>()
     // TODO (02) Change references to score and word to score.value and word.value and add the
     // require null safety checks
 
@@ -41,6 +42,7 @@ class GameViewModel : ViewModel() {
         resetList()
         nextWord()
         // TODO (03) Initialize score.value to 0
+        score.value = 0
     }
 
     /**
@@ -81,19 +83,21 @@ class GameViewModel : ViewModel() {
         if (wordList.isEmpty()) {
             // gameFinished() should happen here
         } else {
-            word = wordList.removeAt(0)
+            word.value = wordList.removeAt(0)
         }
     }
 
     /** Methods for buttons presses **/
 
     fun onSkip() {
-        score--
+//        score--
+        score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+//        score++
+        score.value = (score.value)?.plus(1)
         nextWord()
     }
 }

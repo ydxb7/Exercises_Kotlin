eaf8417b16dca33856937c36ee3991eb0fab0023
Yini <ydxb7@mail.missouri.edu>
2019-07-01 08:57:41 -0700

    06.02 Create SleepDatabaseDao: 使用Dao，把kotlin function和需要操作的database query map到一起。

--- Lesson06-SleepQualityTracker-Database/02-Create-SleepDatabaseDao/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ Lesson06-SleepQualityTracker-Database/02-Create-SleepDatabaseDao/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,20 +16,39 @@
 
 package com.example.android.trackmysleepquality.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
 // TODO (01) Create annotated interface SleepDatabaseDao.
+@Dao
 interface SleepDatabaseDao {
 
-// TODO (02) Add annotated insert() method for inserting a single SleepNight.
+    // TODO (02) Add annotated insert() method for inserting a single SleepNight.
+    @Insert
+    fun insert(night: SleepNight)
 
-// TODO (03) Add annotated update() method for updating a SleepNight.
+    // TODO (03) Add annotated update() method for updating a SleepNight.
+    @Update
+    fun update(night: SleepNight)
 
-// TODO (04) Add annotated get() method that gets the SleepNight by key.
+    // TODO (04) Add annotated get() method that gets the SleepNight by key.
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: Long): SleepNight?
 
-// TODO (05) Add annotated clear() method and query.
+    // TODO (05) Add annotated clear() method and query.
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
 
-// TODO (06) Add annotated getAllNights() method and query.
+    // TODO (06) Add annotated getAllNights() method and query.
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
 
-// TODO (07) Add annotated getTonight() method and query.
+    // TODO (07) Add annotated getTonight() method and query.
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
 
 }
 

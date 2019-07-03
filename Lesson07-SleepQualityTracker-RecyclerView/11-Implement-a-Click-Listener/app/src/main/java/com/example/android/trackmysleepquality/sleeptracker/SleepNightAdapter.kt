801066ae7189774ec28cd9ad784f48011ffc0c6b
Yini <ydxb7@mail.missouri.edu>
2019-07-03 07:21:05 -0700

    L07.11 Implement a Click Listener: 实现recyclerView每个item的点击，用data-binding直接与xml绑定

--- Lesson07-SleepQualityTracker-RecyclerView/11-Implement-a-Click-Listener/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ Lesson07-SleepQualityTracker-RecyclerView/11-Implement-a-Click-Listener/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -25,14 +25,17 @@ import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
 
 // TODO (04) Add a SleepNightListener reference to the SleepNightAdapter class declaration.
-class SleepNightAdapter : ListAdapter<SleepNight,
+//class SleepNightAdapter : ListAdapter<SleepNight,
+//        SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
+class SleepNightAdapter(val clickListener: SleepNightListener) : ListAdapter<SleepNight,
         SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
 
         // TODO (05) Add clickListener parameter to holder.bind().
-        holder.bind(item)
+//        holder.bind(item)
+        holder.bind(clickListener,item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -44,8 +47,10 @@ class SleepNightAdapter : ListAdapter<SleepNight,
 
         // TODO (06) Add a clickListener parameter to the bind() function,
         // and add a binding for the clickListener.
-        fun bind(item: SleepNight) {
+//        fun bind(item: SleepNight) {
+        fun bind(clickListener: SleepNightListener, item: SleepNight) {
             binding.sleep = item
+            binding.clickListener = clickListener
             binding.executePendingBindings()
         }
 
@@ -77,3 +82,6 @@ class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
 }
 
 // TODO (01) Create a new class called SleepNightListener.
+class SleepNightListener(val clickListener: (sleepId: Long) -> Unit) {
+    fun onClick(night: SleepNight) = clickListener(night.nightId)
+}

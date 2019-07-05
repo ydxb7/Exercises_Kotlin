d54deb979bb6fa54f26a10e9fb635e55d5a154f8
Yini <ydxb7@mail.missouri.edu>
2019-07-04 21:39:30 -0700

    L08.06 Error Handling with RecyclerView: 处理error，如果没网络，或连接错误就显示错误图片，如果整个页面在加载，就显示加载中

--- Lesson08-MarsRealEstate-Web/06-Error-Handling-with-RecyclerView/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ Lesson08-MarsRealEstate-Web/06-Error-Handling-with-RecyclerView/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -28,6 +28,8 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 // TODO (01) Create a MarsApiStatus enum with the LOADING, ERROR, and DONE states
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
@@ -35,10 +37,10 @@ class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the most recent response status
     // TODO (02) Change _status to type MarsApiStatus
-    private val _status = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the status String
-    val status: LiveData<String>
+    val status: LiveData<MarsApiStatus>
         get() = _status
 
     // Internally, we use a MutableLiveData, because we will be updating the List of MarsProperty
@@ -73,12 +75,14 @@ class OverviewViewModel : ViewModel() {
             // Get the Deferred object for our Retrofit request
             var getPropertiesDeferred = MarsApi.retrofitService.getProperties()
             try {
+                _status.value = MarsApiStatus.LOADING
                 // this will run on a thread managed by Retrofit
                 val listResult = getPropertiesDeferred.await()
-                _status.value = "Success: ${listResult.size}"
+                _status.value = MarsApiStatus.DONE
                 _properties.value = listResult
             } catch (e: Exception) {
-                _status.value = "Failure: ${e.message}"
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = ArrayList()
             }
         }
     }

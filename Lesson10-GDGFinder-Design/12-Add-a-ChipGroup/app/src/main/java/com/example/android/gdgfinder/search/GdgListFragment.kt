0172bcff29735986775d2837a850363750e2a8ad
Yini <ydxb7@mail.missouri.edu>
2019-07-07 15:41:59 -0700

    L10.12 Add a ChipGroup: 在屏幕上方显示一排选项，每次只能选中一个，选中变色，并执行filter。

--- Lesson10-GDGFinder-Design/12-Add-a-ChipGroup/app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
+++ Lesson10-GDGFinder-Design/12-Add-a-ChipGroup/app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
@@ -15,6 +15,7 @@ import com.example.android.gdgfinder.databinding.FragmentGdgListBinding
 import com.google.android.gms.location.*
 import com.google.android.material.snackbar.Snackbar
 import com.example.android.gdgfinder.R
+import com.google.android.material.chip.Chip
 
 private const val LOCATION_PERMISSION_REQUEST = 1
 
@@ -60,15 +61,34 @@ class GdgListFragment : Fragment() {
 
         // TODO (04) Create an observer on viewModel.regionList. Override the required
         // onChanged() method to include the following changes.
+        viewModel.regionList.observe(viewLifecycleOwner, object: Observer<List<String>> {
+            override fun onChanged(data: List<String>?) {
+                data ?: return
+                // TODO (05) Create a new layoutInflator from the ChipGroup.
+                val chipGroup = binding.regionList
+                val inflator = LayoutInflater.from(chipGroup.context)
 
-        // TODO (05) Create a new layoutInflator from the ChipGroup.
+                // TODO (06) Use the map() function to create a Chip for each item in regionList and
+                // return the results as a new list called children.
+                val children = data.map { regionName ->
+                    val chip = inflator.inflate(R.layout.region, chipGroup, false) as Chip
+                    chip.text = regionName
+                    chip.tag = regionName
+                    chip.setOnCheckedChangeListener { button, isChecked ->
+                        viewModel.onFilterChanged(button.tag as String, isChecked)
+                    }
+                    chip
+                }
 
-        // TODO (06) Use the map() function to create a Chip for each item in regionList and
-        // return the results as a new list called children.
+                // TODO (07) Call chipGroup.removeAllViews() to remove any views already in chipGroup.
+                chipGroup.removeAllViews()
 
-        // TODO (07) Call chipGroup.removeAllViews() to remove any views already in chipGroup.
-
-        // TODO (08)  Iterate through the list of children and add each chip to chipGroup.
+                // TODO (08)  Iterate through the list of children and add each chip to chipGroup.
+                for (chip in children) {
+                    chipGroup.addView(chip)
+                }
+            }
+        })
 
         setHasOptionsMenu(true)
         return binding.root
